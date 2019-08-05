6f066ab723b87f7a8914434bbb8acbbb14b3b8db
Gloria Budiman <wahyu.g@gmail.com>
2019-08-05 03:32:07 -0700

    Controller stat test

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,9 +1,16 @@
 class CharactersController < ApplicationController
   def xp
-    char = Character.new(skill_ids: params[:skill_ids])
+    char = Character.new(
+      skill_ids: params[:skill_ids] || [],
+      body: params[:body] || 0,
+      mind: params[:mind] || 0,
+      resolve: params[:resolve] || 0,
+      infection: params[:infection] || 0,
+    )
     char.validates_dependencies
     errors = char.errors.messages
     render json: {
+      stat_xp: char.compute_stat_xp,
       skill_xp: errors.length > 0 ? nil : char.compute_skill_xp,
       errors: errors,
     }
--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe CharactersController, type: :controller do
   let(:response_body) { JSON.parse(response.body).deep_symbolize_keys }
 
-  it 'responds properly' do
+  it 'responds to skill XP properly' do
     post(:xp, params: { skill_ids: [1,2,3] })
     expect(response_body[:skill_xp]).to eq(12)
     expect(response_body[:errors].length).to eq(0)
@@ -13,5 +13,11 @@ RSpec.describe CharactersController, type: :controller do
     post(:xp, params: { skill_ids: [1,21] })
     expect(response_body[:skill_xp]).to eq(nil)
     expect(response_body[:errors].length).to eq(1)
+  end
+
+  it 'responds to stat XP properly' do
+    post(:xp, params: { body: 7, mind: 7, resolve: 1, infection: 1 })
+    expect(response_body[:stat_xp]).to eq(34)
+    expect(response_body[:errors].length).to eq(0)
   end
 end
